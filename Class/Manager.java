1a0a6196f48fa39d8e10f3d6b1cf6714434f961d
Valentin0811 <vlaurent811@gmail.com>
2026-04-10 10:34:41 +0200

    class

--- Class/Manager.java
+++ Class/Manager.java
@@ -1,13 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Manager extends User {
-	private String teamId;
-	private List<User> teamMembers = new ArrayList<>();
+	private String department;
 	
-	public Manager(String id, String name, String email, String teamId) {
+	public Manager(String id, String name, String email, String department) {
 		super(id, name, email);
-		this.teamId = teamId;
+		this.department = department;
 	}
 	public boolean canCreateTask() {
 		return true;
@@ -16,25 +12,13 @@ public class Manager extends User {
 		return false;
 	}
 	public boolean canAssignTask() {
-		return false;
+		return true;
 	}
-	public void addTeamMembers(User user) {
-		teamMembers.add(user);
+	public String getDepartment() {
+		return department;
 	}
-	public List<User> getTeamMembers() {
-		return teamMembers;	
+	public void setDepartment(String department) {
+		this.department = department;
 	}
-	public void reviewTeamProgress(TaskManager manager) {
-		System.out.println("Progression de l'équipe" + teamId + ":");
-		
-		for (User member : teamMembers) {
-			System.out.println("-" + member.name + ":");
-			
-			for (Task t : manager.getAllTasks()) {
-				if (t.getAssignedEngineer() != null && t.getAssignedEngineer().equals(member.name)) {
-					System.out.println("." + t.getTitle() + "[" + t.getStatus() + "]" );
-				}
-			}
-		}
-	}
+
 }
