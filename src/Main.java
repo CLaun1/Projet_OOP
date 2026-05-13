69ddc054464bb58d0051d1d274732570ab26802e
kingshasha11 <louismvemba@gmail.com>
2026-05-13 10:29:02 +0200

    Configuration JavaFX

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+package src;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Ça marche !");
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("Test JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
